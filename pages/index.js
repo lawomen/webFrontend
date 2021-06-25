12370eb94ef415eb67dacac5ae213376c8739e00
lawomen <tech.team@lawomen.pk>
2021-06-24 22:28:40 -0700

    landing page minor change

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,15 @@ import LawArea from "../components/landing/LawArea";
 import WhyLawomen from "../components/landing/WhyLawomen";
 import Team from "../components/landing/Team";
 
-import { useEffect } from "react";
+import {
+  RiInstagramLine,
+  RiFacebookCircleFill,
+  RiLinkedinFill,
+} from "react-icons/ri";
+
+import { BsFillCaretRightFill } from "react-icons/bs";
+
+import { useEffect, useRef } from "react";
 import Image from "next/image";
 import homeStyle from "../styles/Home.module.css";
 
@@ -38,6 +46,8 @@ function Home({ apiRes }) {
   const t1 = useTranslation("landing");
   const t2 = useTranslation("common");
 
+  const contactUs = useRef(null);
+
   useEffect(() => {
     // to call the heroku, deal with cold start so form submit is faster
     try {
@@ -61,19 +71,46 @@ function Home({ apiRes }) {
             <h1>{t2.t("companyName")}</h1>
             <h2>{apiRes.tagline}</h2>
             <h3>{apiRes.landingDesc}</h3>
+            <button
+              className={homeStyle.contact}
+              onClick={() => {
+                contactUs.current.scrollIntoView({
+                  behavior: "smooth",
+                  block: "center",
+                });
+              }}
+            >
+              Contact Us
+              <BsFillCaretRightFill size={27} />
+            </button>
           </div>
         </div>
+        <div className={homeStyle.socials}>
+          <a target="_blank" href="https://www.facebook.com/LawomenPk/">
+            <RiFacebookCircleFill size={40} />
+          </a>
+          <a target="_blank" href="https://www.instagram.com/lawomenpk/">
+            <RiInstagramLine size={40} />
+          </a>
+          <a target="_blank" href="https://pk.linkedin.com/company/lawomen">
+            <RiLinkedinFill size={40} />
+          </a>
+        </div>
       </section>
+
       <section>
         <LawArea />
       </section>
+
       <section>
         <WhyLawomen />
       </section>
+
       <section>
         <Team />
       </section>
-      <section>
+
+      <section ref={contactUs}>
         <h3>Let us know how we can help</h3>
         <FormikForm />
       </section>
